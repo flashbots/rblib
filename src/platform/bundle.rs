cb12a5d8a0a09da1e1203f383281112ac450af51
julio4 <30329843+julio4@users.noreply.github.com>
2025-09-24 10:38:14 +0900

    fix: bundle Eligibility as bool

--- src/platform/bundle.rs
+++ src/platform/bundle.rs
@@ -137,34 +137,6 @@ pub enum Eligibility {
 	PermanentlyIneligible,
 }
 
-/// This is a quality of life helper that allows users of this api to say:
-/// `if bundle.is_eligible(block) { .. }`, without going into the details of
-/// the eligibility.
-impl PartialEq<bool> for Eligibility {
-	fn eq(&self, other: &bool) -> bool {
-		match self {
-			Eligibility::Eligible => *other,
-			Eligibility::TemporarilyIneligible
-			| Eligibility::PermanentlyIneligible => !(*other),
-		}
-	}
-}
-
-/// This is a quality of life helper that allows users of this api to say:
-/// `if !bundle.is_eligible(block) { .. }`, without going into the details of
-/// the ineligibility.
-impl Not for Eligibility {
-	type Output = bool;
-
-	fn not(self) -> Self::Output {
-		match self {
-			Eligibility::Eligible => false,
-			Eligibility::TemporarilyIneligible
-			| Eligibility::PermanentlyIneligible => true,
-		}
-	}
-}
-
 impl Eligibility {
 	/// Returns `Some(f())` if the eligibility is `Eligible`, otherwise returns
 	/// `None`.
@@ -184,6 +156,24 @@ impl Eligibility {
 		} else {
 			None
 		}
+	}
+
+	/// This is a quality of life helper that allows users of this api to say:
+	/// `if bundle.is_eligible(block).as_bool() { .. }`, without going into the
+	/// details of the eligibility.
+	pub fn as_bool(self) -> bool {
+		matches!(self, Eligibility::Eligible)
+	}
+}
+
+/// This is a quality of life helper that allows users of this api to say:
+/// `if !bundle.is_eligible(block) { .. }`, without going into the details of
+/// the ineligibility.
+impl Not for Eligibility {
+	type Output = bool;
+
+	fn not(self) -> Self::Output {
+		!self.as_bool()
 	}
 }
 
